979ef62b83d5da90bd031e2b7474249c15c39194
Anmolsoni04 <anmolsoni1004@gmail.com>
2024-05-21 16:08:17 +0530

    add image in cards

--- M03 Components Props and JSX/first-app/src/components/Card.jsx
+++ M03 Components Props and JSX/first-app/src/components/Card.jsx
@@ -4,7 +4,8 @@ import "./Card.css"
 const Card = (props) => {
   return ( 
       <div className="title">
-        <p>{props.p}</p>      
+        <p>{props.p}</p> 
+        <img src="https://www.shutterstock.com/shutterstock/photos/1932042689/display_1500/stock-photo-businessman-using-mobile-smart-phone-business-global-internet-connection-application-technology-1932042689.jpg" alt="" />     
       </div>
     )
 }
